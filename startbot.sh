9415ce5f86b47b0ed50b763f9cfd4d859e320c0d
Amaechi Abuah <amaechiabuah@Amaechis-MacBook-Pro.local>
2022-02-08 09:13:24 +0100

    m

--- startbot.sh
+++ startbot.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-git add .
-git commit -m"m"
-git push heroku master
-pyinstaller --onefile abuahfish/abuahfish.py
-chmod a+x /dist/abuahfish
+chmod a+x /engines/abuahfish
 python lichess-bot.py
